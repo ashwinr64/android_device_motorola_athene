3656b46597ee12e15dccfaff7b1eab95119d1aa8
Ashwin Ramesh <ashwinr64@gmail.com>
2016-08-11 22:30:53 +0530

    athene: Unify Moto G4 / G4 plus
    * Both devices work with some blobs , no point in seperate tree. So
    Unify.

--- cm.mk
+++ cm.mk
@@ -1,11 +1,10 @@
-#
 # Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter athene, $(TARGET_DEVICE)),)
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-LOCAL_PATH := $(call my-dir)
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-endif
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := athene
+PRODUCT_NAME := cm_athene
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := athene
